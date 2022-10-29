5fe71c0616b546895a2954ac8e39de4649758372
geronimovillaverde <94569351+geronimovillaverde@users.noreply.github.com>
2022-10-29 17:16:53 -0300

    Agrego mis datos

--- programaFuentesLaForgiaMachaique.php
+++ programaFuentesLaForgiaMachaique.php
@@ -7,6 +7,7 @@ include_once("wordix.php"); //bibloteca de funcion, incluya por unica vez el arc
 
 /* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
 /* Floriana Daniela La Forgia - Legajo 2498 - mail: florianalaforgia@hotmail.com.ar - Github: Alter1412   
+/*Geronimo Villaverde - Legajo 3536 - Mail: geronimovillaverde@gmail.com - Github: geronimovillaverde
 */
 
 
