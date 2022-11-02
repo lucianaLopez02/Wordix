7aa47e04429fb9c4d30cb8f768e8aa7e9d09cc5d
Floriana La Forgia <florianalaforgia@hotmail.com.ar>
2022-11-02 18:55:59 -0300

    corrección menor

--- programaFuentesLaForgiaMachaiqueVillaverde.php
+++ programaFuentesLaForgiaMachaiqueVillaverde.php
@@ -138,7 +138,7 @@ return $nuevaColeccionPalabras;
 }
 
 
-//Punto 8 resumen jugador
+//Punto 8 
 /** Funcion que retorna el indice de la primera partida ganada
  * @param array $coleccionPartidas
  * @param string $nombreJugador
